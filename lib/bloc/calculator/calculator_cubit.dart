93abdd9fcb29072e406b4ff869b4531528ba6628
cech5 <carlos_7174@hotmail.com>
2023-02-02 01:02:29 -0500

    Create addDigit method into cubit

--- lib/bloc/calculator/calculator_cubit.dart
+++ lib/bloc/calculator/calculator_cubit.dart
@@ -1,10 +1,13 @@
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 
 part 'calculator_state.dart';
 
 class CalculatorCubit extends Cubit<CalculatorState> {
   CalculatorCubit() : super(const CalculatorState());
 
-
+  void addDigit(String digit) {
+    
+  }
 
 }
