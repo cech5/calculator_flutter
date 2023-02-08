d3a332b5bfb575ee8c17743706c2a7d7c13e17d2
cech5 <carlos_7174@hotmail.com>
2023-02-07 23:21:40 -0500

    Add new instance of calculator cubit

--- lib/features/calculator/views/calculator_page.dart
+++ lib/features/calculator/views/calculator_page.dart
@@ -1,6 +1,7 @@
+import 'package:calculator_flutter/features/calculator/bloc/calculator_cubit.dart';
+import 'package:calculator_flutter/features/calculator/views/carculator_view.dart';
 import 'package:flutter/material.dart';
-
-import 'carculator_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CalculatorPage extends StatelessWidget {
   const CalculatorPage({super.key});
@@ -11,6 +12,9 @@ class CalculatorPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CalculatorView();
+    return BlocProvider(
+      create: (context) => CalculatorCubit(),
+      child: const CalculatorView(),
+    );
   }
 }
