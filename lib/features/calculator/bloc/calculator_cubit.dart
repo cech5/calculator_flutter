d79d2f8c93087994f7b859f9366e9f62c7e6f55a
cech5 <carlos_7174@hotmail.com>
2023-02-07 23:17:19 -0500

    Create methods to update state

--- lib/features/calculator/bloc/calculator_cubit.dart
+++ lib/features/calculator/bloc/calculator_cubit.dart
@@ -6,8 +6,28 @@ part 'calculator_state.dart';
 class CalculatorCubit extends Cubit<CalculatorState> {
   CalculatorCubit() : super(const CalculatorState());
 
-  void addDigit(String digit) {
-    
+  void addDigitToFirstNumber(String digit) {
+    emit(state.copyWith(firstNumber: '${state.firstNumber}$digit'));
   }
 
+  void addOperator(String operator) {
+    emit(state.copyWith(operation: operator));
+  }
+
+  void addDigitToSeconNumber(String digit) {
+    emit(state.copyWith(secondNumber: '${state.secondNumber}$digit'));
+  }
+
+  void addResult() {
+    final firstNumberInt = int.parse(state.firstNumber);
+    final secondNumberInt = int.parse(state.secondNumber);
+    final result;
+    switch (state.operation) {
+      case '+':
+        result = firstNumberInt + secondNumberInt;
+        emit(state.copyWith(mathResult: result.toString()));
+        break;
+      default:
+    }
+  }
 }
