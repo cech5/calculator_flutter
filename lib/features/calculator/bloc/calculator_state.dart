d029dc097e5034683e7824186395684144839203
cech5 <carlos_7174@hotmail.com>
2023-02-09 22:19:03 -0500

    Add new hasSign property in calculator state

--- lib/features/calculator/bloc/calculator_state.dart
+++ lib/features/calculator/bloc/calculator_state.dart
@@ -6,26 +6,36 @@ class CalculatorState extends Equatable {
     this.firstNumber = '',
     this.secondNumber = '',
     this.operation = '',
+    this.hasSign = false,
   });
 
   final String mathResult;
   final String firstNumber;
   final String secondNumber;
   final String operation;
+  final bool hasSign;
 
   @override
-  List<Object> get props => [mathResult, firstNumber, secondNumber, operation];
+  List<Object> get props => [
+        mathResult,
+        firstNumber,
+        secondNumber,
+        operation,
+        hasSign,
+      ];
 
   CalculatorState copyWith({
     String? mathResult,
     String? firstNumber,
     String? secondNumber,
     String? operation,
+    bool? hasSign,
   }) =>
       CalculatorState(
         firstNumber: firstNumber ?? this.firstNumber,
         mathResult: mathResult ?? this.mathResult,
         secondNumber: secondNumber ?? this.secondNumber,
         operation: operation ?? this.operation,
+        hasSign: hasSign ?? this.hasSign,
       );
 }
