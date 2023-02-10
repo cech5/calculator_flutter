ad61ac3508c2c4e224d075436f9cd5e03591dc9f
cech5 <carlos_7174@hotmail.com>
2023-02-09 23:02:58 -0500

    Delete hasSign property in calculator state

--- lib/features/calculator/bloc/calculator_state.dart
+++ lib/features/calculator/bloc/calculator_state.dart
@@ -6,36 +6,26 @@ class CalculatorState extends Equatable {
     this.firstNumber = '',
     this.secondNumber = '',
     this.operation = '',
-    this.hasSign = false,
   });
 
   final String mathResult;
   final String firstNumber;
   final String secondNumber;
   final String operation;
-  final bool hasSign;
 
   @override
-  List<Object> get props => [
-        mathResult,
-        firstNumber,
-        secondNumber,
-        operation,
-        hasSign,
-      ];
+  List<Object> get props => [mathResult, firstNumber, secondNumber, operation];
 
   CalculatorState copyWith({
     String? mathResult,
     String? firstNumber,
     String? secondNumber,
     String? operation,
-    bool? hasSign,
   }) =>
       CalculatorState(
         firstNumber: firstNumber ?? this.firstNumber,
         mathResult: mathResult ?? this.mathResult,
         secondNumber: secondNumber ?? this.secondNumber,
         operation: operation ?? this.operation,
-        hasSign: hasSign ?? this.hasSign,
       );
 }
