a3cf8ad8a3c69380d07e23bd4c6cb4aaf2c9cab2
cech5 <carlos_7174@hotmail.com>
2023-02-12 19:32:41 -0500

    Add blocprovider and blocbuilder to render theme

--- lib/features/app/view/app.dart
+++ lib/features/app/view/app.dart
@@ -1,18 +1,42 @@
+import 'package:calculator_flutter/features/calculator/calculator.dart';
+import 'package:calculator_flutter/features/theme/theme.dart';
 import 'package:calculator_flutter/l10n/l10n.dart';
-import 'package:calculator_flutter/features/calculator/views/calculator_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark(),
-      debugShowCheckedModeBanner: false,
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      home: const CalculatorPage(),
+    return BlocProvider(
+      create: (context) => AppThemeCubit(),
+      child: BlocBuilder<AppThemeCubit, ThemeData>(
+        builder: (context, state) {
+          return MaterialApp(
+            builder: (context, child) => ResponsiveWrapper.builder(
+              BouncingScrollWrapper.builder(context, child!),
+              maxWidth: 393,
+              minWidth: 392,
+              defaultScale: true,
+              breakpoints: [
+                const ResponsiveBreakpoint.resize(450, name: MOBILE),
+                const ResponsiveBreakpoint.autoScale(800, name: TABLET),
+                const ResponsiveBreakpoint.autoScale(1000, name: TABLET),
+                const ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+                const ResponsiveBreakpoint.autoScale(2460, name: "4K"),
+              ],
+              background: Container(color: const Color(0xFFF5F5F5)),
+            ),
+            theme: state,
+            debugShowCheckedModeBanner: false,
+            localizationsDelegates: AppLocalizations.localizationsDelegates,
+            supportedLocales: AppLocalizations.supportedLocales,
+            home: const CalculatorPage(),
+          );
+        },
+      ),
     );
   }
 }
